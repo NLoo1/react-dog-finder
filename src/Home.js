82188ee3a9102717f6668751e8114b39c80811c6
Nicholas <NLoo1@outlook.com>
2024-07-03 17:52:25 -0400

    Remove nav, add dog imgs and CSS

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import { Route, Routes, Link, Navigate, useLocation } from "react-router-dom";
-import { Dog } from './routesList';
+import { DogDetails } from './routesList';
 import './Home.css'
 
 function Home() {
   const location = useLocation();
   return (
     <div className="VendingMachine">
-      <nav>
+      {/* <nav>
         {location.pathname == '/dogs' &&
             <div>
                 <Link to="/dogs/duke" >Duke</Link>
@@ -15,13 +15,19 @@ function Home() {
             </div>
             
         }
-      </nav>
+      </nav> */}
       {location.pathname === '/dogs' &&
         <h1>Choose a dog</h1>}
       <Routes>
-        <Route path='/dogs/:name' element={<Dog />} />
+        <Route exact path='/dogs/:name' element={<DogDetails />} />
         <Route path="*" element={<Navigate to="/dogs" />} /> 
       </Routes>
+
+      <div className="dogs">
+        <img src="./duke.jpg" />
+        <img src="./perry.jpg" />
+        <img src="./whiskey.jpg" />
+      </div>
     </div>
   );
 }
--- src/routesList.js
+++ src/routesList.js
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import data from './db.json'
 
 
-export function Dog(){
+export function DogDetails(){
     const params = useParams()
     const dog = data.dogs.find(params.name)
     return (<div>
