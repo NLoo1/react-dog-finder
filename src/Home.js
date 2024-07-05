360fc900f445835f36461d6abc325a6ba66b3b18
Nicholas L <NLoo1@outlook.com>
2024-07-05 16:22:05 -0400

    Added routing to show dog details

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,7 @@
 import { Route, Routes, Link, Navigate, useLocation } from "react-router-dom";
 import { DogDetails } from './routesList';
 import './Home.css'
-import dukeImage from './images/duke.jpg';
-import perryImage from './images/perry.jpg';
-import whiskeyImage from './images/whiskey.jpg';
+
 
 function Home() {
   const location = useLocation();
@@ -11,18 +9,19 @@ function Home() {
     <div className="VendingMachine">
 
       {location.pathname === '/dogs' &&
-        <h1>Choose a dog</h1>}
+        <h1 classname="choose-dog">Choose a dog</h1>}
       <Routes>
         <Route exact path='/dogs/:name' element={<DogDetails />} />
         <Route path="*" element={<Navigate to="/dogs" />} /> 
       </Routes>
-
+ 
+      { location.pathname == '/dogs' &&
       <div className="dogs">
-        {console.log(location.pathname)}
-        <Link to="/dogs/duke" ><img src={dukeImage} /></Link>
-        <Link to="/dogs/perry" ><img src={perryImage} /></Link>
-        <Link to="/dogs/whiskey" ><img src={whiskeyImage} /></Link>
-      </div>
+      <Link to="/dogs/duke" ><img src={process.env.PUBLIC_URL + '/duke.jpg'} /></Link>
+      <Link to="/dogs/perry" ><img src={process.env.PUBLIC_URL + '/perry.jpg'} /></Link>
+      <Link to="/dogs/whiskey" ><img src={process.env.PUBLIC_URL + '/whiskey.jpg'} /></Link>
+    </div>
+      }
     </div>
   );
 }
